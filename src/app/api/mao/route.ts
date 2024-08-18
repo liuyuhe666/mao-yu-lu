a8191cbafbd4ccab12d40842b85504f8a7b509e2
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-08-18 18:53:56 +0800

    feat: update code

---
+++ src/app/api/mao/route.ts
@@ -0,0 +1,16 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+
+export async function GET() {
+  const filePath = path.join(process.cwd(), 'public', 'data', 'maoyulu.txt')
+  const fileContent = fs.readFileSync(filePath, 'utf-8')
+  const textList = fileContent.split('\n')
+  const index = Math.floor(Math.random() * textList.length)
+  const text = textList[index].trim()
+  return Response.json({
+    code: 0,
+    msg: '成功',
+    data: text,
+  })
+}
