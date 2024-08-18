85be8afbbd806720bd98fff414f0870f0494b9b4
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-08-18 20:04:26 +0800

    fix: no store

--- src/app/api/mao/route.ts
+++ src/app/api/mao/route.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
+import { NextResponse } from 'next/server'
 
 const filePath = path.join(process.cwd(), 'public', 'data', 'maoyulu.txt')
 const fileContent = fs.readFileSync(filePath, 'utf-8')
@@ -9,9 +10,13 @@ const textList = fileContent.split('\n')
 export async function GET() {
   const index = Math.floor(Math.random() * textList.length)
   const text = textList[index].trim()
-  return Response.json({
+  return NextResponse.json({
     code: 0,
     msg: '成功',
     data: text,
+  }, {
+    headers: {
+      'Cache-Control': 'no-store', // 完全不缓存数据
+    },
   })
 }
