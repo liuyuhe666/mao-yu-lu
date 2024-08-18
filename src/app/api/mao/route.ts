90facea344915bc9eadfd945580ddb0237e58c2b
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-08-18 19:50:46 +0800

    fix: data cache

--- src/app/api/mao/route.ts
+++ src/app/api/mao/route.ts
@@ -2,10 +2,11 @@ import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
 
+const filePath = path.join(process.cwd(), 'public', 'data', 'maoyulu.txt')
+const fileContent = fs.readFileSync(filePath, 'utf-8')
+const textList = fileContent.split('\n')
+
 export async function GET() {
-  const filePath = path.join(process.cwd(), 'public', 'data', 'maoyulu.txt')
-  const fileContent = fs.readFileSync(filePath, 'utf-8')
-  const textList = fileContent.split('\n')
   const index = Math.floor(Math.random() * textList.length)
   const text = textList[index].trim()
   return Response.json({
