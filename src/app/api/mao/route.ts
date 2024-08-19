c82d09f85b3235fe7357a6f64a6895cec63e5ebd
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-08-19 09:12:27 +0800

    feat: pagination

--- src/app/api/mao/route.ts
+++ src/app/api/mao/route.ts
@@ -1,9 +1,35 @@
+import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 import prisma from '@/lib/db'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const page = searchParams.get('page')
+  const pageSize = searchParams.get('size')
   // 获取总数
   const count = await prisma.mao.count()
+  if (page && pageSize) {
+    try {
+      const result = await prisma.mao.findMany({
+        skip: (Number(page) - 1) * Number(pageSize),
+        take: Number(pageSize),
+      })
+      return NextResponse.json({
+        code: 0,
+        msg: '成功',
+        data: result,
+        total: count,
+      })
+    }
+    catch (e) {
+      console.error(e)
+      return NextResponse.json({
+        code: -1,
+        msg: '失败',
+        data: null,
+      })
+    }
+  }
   // 生成一个随机索引
   const randomIndex = Math.floor(Math.random() * count)
   // 根据随机索引获取数据
